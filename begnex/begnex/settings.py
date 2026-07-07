16aeb40521b9f7f74f1a58be2e593ee11763a067
Fayis <shamonfayis@gmail.com>
2026-07-07 17:21:24 +0530

    Update ALLOWED_HOSTS

--- begnex/begnex/settings.py
+++ begnex/begnex/settings.py
@@ -17,8 +17,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "13.60.96.33",
+]
 
 # Application definition
 
